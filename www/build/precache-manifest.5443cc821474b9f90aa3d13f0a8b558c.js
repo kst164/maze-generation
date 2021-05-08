e3d0d2c2a9841122c6f21f1a27984bc149f79eb2
kst164 <kartheek@tammana.org>
2021-05-08 18:29:44 +0530

    Including react prod build

---
+++ www/build/precache-manifest.5443cc821474b9f90aa3d13f0a8b558c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6f61f6129c79776b8b",
+    "url": "/48aa0f04e1cb9b7fd3ae.module.wasm"
+  },
+  {
+    "revision": "4c62ba77cff75c21ad9f1bbe9eba176f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db4e23195719a8ace503",
+    "url": "/static/css/main.02d6bab8.chunk.css"
+  },
+  {
+    "revision": "b73687d0ce43b45ac325",
+    "url": "/static/js/2.5e4b1df0.chunk.js"
+  },
+  {
+    "revision": "ce6f61f6129c79776b8b",
+    "url": "/static/js/3.7eac64d3.chunk.js"
+  },
+  {
+    "revision": "db4e23195719a8ace503",
+    "url": "/static/js/main.75179fa5.chunk.js"
+  },
+  {
+    "revision": "a361cf922991685f12ff",
+    "url": "/static/js/runtime-main.784735dd.js"
+  }
+]);
